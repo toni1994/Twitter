9d9240015d525edf383a211ac10e355f043db65a
mario1994 <mario.boban94@gmail.com>
2016-12-07 12:38:01 +0100

    added Toni's navigation bar and css styles

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,34 +6,48 @@ import { Component } from '@angular/core';
   template: `
    <nav class="navbar navbar-default">
   <div class="container-fluid">
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+  <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+
+
+    <div class="collapse navbar-collapse" id="bs-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">Naslovnica <i class="fa fa-home fa-lx" aria-hidden="true"></i> </a></li>
         <li><a href="#">Obavijesti <i class="fa fa-bell fa-lx" aria-hidden="true"></i> </a></li>
-		    <li><a href="#">Poruke <i class="fa fa-envelope fa-lx" aria-hidden="true"></i> </a></li>  
+      <li><a href="#">Poruke <i class="fa fa-envelope fa-lx" aria-hidden="true"></i> </a></li>  
       </ul>
       <ul class="nav navbar-nav navbar-right">
-	  <form class="navbar-form navbar-left">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-	  
-	  <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+<form class="navbar-form navbar-left" role="search">
+    <div class="form-group">
+        <input type="text" class="form-control" placeholder="Search">
+    </div>
+    <button id="searchIcon" type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+</form>
+   
+   <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Profile <i class="fa fa-user" aria-hidden="true"></i>  <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
+            <li><a href="#">Moj profil</a></li>
             <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
+            <li><a href="#">Popisi</a></li>
+            <li><a href="#">Aktualni događaji</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Pomoć</a></li>
+            <li><a href="#">Tikovni prečaci</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Postvake</a></li>
+            <li><a href="#">Odjava</a></li>
           </ul>
         </li>
-		
-    
-       <button type="submit" class="btn btn-primary">Tweet</button>
-       
+  
+       <button type="submit" class="btn btn-primary navbar-btn"><i class="fa fa-pencil" aria-hidden="true"></i> Tweet</button>
         
       </ul>
     </div><!-- /.navbar-collapse -->
