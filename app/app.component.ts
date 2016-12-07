9589fe6e69f71ec6cd1f4499da10c8998f5b1933
mario1994 <mario.boban94@gmail.com>
2016-12-07 12:25:35 +0100

    created right Who to follow panel

--- app/app.component.ts
+++ app/app.component.ts
@@ -147,7 +147,59 @@ import { Component } from '@angular/core';
         </div>
       </div>
         </div>
-      </div>           
+      </div>  
+
+      <!--THIS IS THE RIGHT COLUMN AKA FOLLOW COLUMN-->
+
+<div class="col-md-3">
+   <div class="panel panel-default text-center">
+      <div class="panel-body">
+        <div><strong>Koga Pratiti</strong></div>
+        <div class="row">
+        <hr>
+            <div class="col-xs-3">
+               <div class="big-box">
+                  <img src="https://pbs.twimg.com/profile_images/785814072075907072/ZoMi_QkU_normal.jpg" class="img-thumbnail" >
+               </div>
+            </div>
+            <div class="col-xs-6">
+               <div class="row text_left">
+                  <div><strong>NK Osijek</strong> @nkosijek</div>
+                  <a class="btn btn-default follow-btn" href="#" role="button"> <i class="fa fa-plus" aria-hidden="true"></i> Slijedi</a>
+               </div>
+            </div>
+         </div>
+         <hr>
+         <div class="row">
+            <div class="col-xs-3">
+               <div class="big-box">
+                  <img src="https://pbs.twimg.com/profile_images/686576914916597760/OY475ev3_normal.png" class="img-thumbnail" >
+               </div>
+            </div>
+            <div class="col-xs-6">
+               <div class="row text_left">
+                  <div><strong>Naš Hajduk</strong> @nas_hajduk</div>
+                  <a class="btn btn-default follow-btn" href="#" role="button"> <i class="fa fa-plus" aria-hidden="true"></i> Slijedi</a>
+               </div>
+            </div>
+         </div>
+         <hr>
+         <div class="row">
+            <div class="col-xs-3">
+               <div class="big-box">
+                  <img src="https://pbs.twimg.com/profile_images/748482794205122560/_xKDpDIv_normal.jpg" class="img-thumbnail" >
+               </div>
+            </div>
+            <div class="col-xs-6">
+               <div class="row text_left">
+                  <div><strong>FK Crvena zvezda</strong> @FKCZ</div>
+                  <a class="btn btn-default follow-btn" href="#" role="button"> <i class="fa fa-plus" aria-hidden="true"></i> Slijedi</a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>  
    `
 })
 export class AppComponent { 
